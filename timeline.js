f8bb673e881600399db777ccfc50b7c6e4c51afa
Loren Rachelle Hicks <lorenhicks@gmail.com>
2020-10-25 23:13:05 -0400

    Assassin's Creed Custom Map API

--- timeline.js
+++
@@ -1,4 +0,0 @@
-$(".timeline-wrapper .timeline-content-item > span").on("mouseenter mouseleave", function(e){
-  $(".timeline-wrapper .timeline-content-item.active").removeClass("active");
-  $(this).parent().addClass("active");
-});
